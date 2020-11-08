a7aac54b595416d52c42c9b9458754af2b8f36f8
likunshang <shanglihero@163.com>
2020-11-08 21:08:12 +0800

    Initial commit

---
+++ foode-dev-api/src/main/java/org/xiaoke/Application.java
@@ -0,0 +1,14 @@
+package org.xiaoke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//启动类
+@SpringBootApplication
+public class Application {
+    // 快速输入psvm
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+
+    }
+}
---
+++ foode-dev-api/src/main/java/org/xiaoke/controller/helloController.java
@@ -0,0 +1,13 @@
+package org.xiaoke.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class helloController {
+
+    @GetMapping("/hello")
+    public Object hello(){
+        return "hello world..";
+    }
+}
